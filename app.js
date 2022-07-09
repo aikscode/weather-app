8834effecdeeb9e65ef2720c9c1ec223baa0c232
Aikins Dwamena <aikinsakenten@gmail.com>
2022-07-09 05:35:39 -0700

    i set up a http request for getting the geocode which will be used in the overall app later on

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 const request = require("request");
 
-const API_KEY = "c366a1aee90d3e06ecf8b38f0896394b";
-const url = `http://api.weatherstack.com/current?access_key=${API_KEY}&query=37.267,-122.4233&units=f`;
+const API_KEY_WEATHERSTACK = "c366a1aee90d3e06ecf8b38f0896394b";
+const url_weatherStack = `http://api.weatherstack.com/current?access_key=${API_KEY_WEATHERSTACK}&query=37.267,-122.4233&units=f`;
+const API_KEY_POSITIONSTACK = "bda2476a8958e2fd93c63d20769998ba";
+const url_positionStack = `http://api.positionstack.com/v1/forward?access_key=${API_KEY_POSITIONSTACK}&query=1600%20Pennsylvania%20Ave%20NW,%20Washington%20DC`;
 
-request({ url: url, json: true }, (error, response) => {
-  const data = response.body;
+request({ url: url_weatherStack, json: true }, (error, response) => {
+  const dataWeather = response.body;
   console.log(
-    data.current.weather_descriptions[0] +
+    dataWeather.current.weather_descriptions[0] +
       " it's currently " +
-      data.current.temperature +
+      dataWeather.current.temperature +
       " degrees out. But it feels like " +
-      data.current.feelslike +
+      dataWeather.current.feelslike +
       " degrees"
+  );
+});
+
+request({ url: url_positionStack, json: true }, (error, response) => {
+  const dataGeocode = response.body.data[0];
+
+  console.log(
+    "The longitude is " +
+      dataGeocode.longitude +
+      " and the latitude is " +
+      dataGeocode.latitude
   );
 });
